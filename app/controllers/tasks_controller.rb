71b23225c85c8e3269b5162f1ac7b011479841e9
shenils <shenil@bu.edu>
2015-10-06 16:29:38 +0530

    add simple form

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,23 @@
 class TasksController < ApplicationController
   def index
+    @tasks = Task.all
   end
 
   def new
     @task = Task.new
   end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save!
+    redirect_to root_path
+  end
+
+
+private
+  
+  def task_params
+    params.require(:task).permit(:title, :description)
+  end
+
 end
